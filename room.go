282a869e22e4aabf2e9a3603084a6172ec3f584c
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-08-04 21:39:09 +0200

    pass book room test

--- room.go
+++ room.go
@@ -11,10 +11,26 @@ func NewDate(year int, month time.Month, day int) Date {
 	return Date{date: time.Date(year, month, day, 0, 0, 0, 0, loc)}
 }
 
-type Room struct {
-	name string
-}
+type Room string
 
 func GetFreeRooms(arrival Date, departure Date) []Room {
-	return []Room{Room{"Rio"}, Room{"Berlin"}}
+	return []Room{"Rio", "Berlin"}
+}
+
+type Booking struct {
+	id        int
+	name      string
+	arrival   Date
+	departure Date
+}
+type Hotel struct {
+	rooms []Room
+}
+
+func (h Hotel) BookARoom(booking Booking) error {
+	return nil
+}
+
+func (h Hotel) GetFreeRooms(arrival Date, departure Date) []Room {
+	return []Room{"Rio"}
 }
--- room_test.go
+++ room_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestShowFreeRooms(t *testing.T) {
-	rooms := []Room{Room{"Rio"}, Room{"Berlin"}}
+	rooms := []Room{"Amsterdam", "Berlin"}
 	arrival := NewDate(2020, time.August, 13)
 	departure := NewDate(2020, time.August, 16)
 	availableRooms := GetFreeRooms(arrival, departure)
@@ -16,11 +16,15 @@ func TestShowFreeRooms(t *testing.T) {
 
 }
 
-// func TestShowNoRooms(t *testing.T) {
-// 	rooms := []Room{Room{"Rio"}, Room{"Berlin"}}
-// 	arrival := NewDate(2020, time.August, 13)
-// 	departure := NewDate(2020, time.August, 16)
-// 	availableRooms := GetFreeRooms(arrival, departure)
-// 	assert.Equal(t, rooms, availableRooms)
+func TestBookRoom(t *testing.T) {
+	arrival := NewDate(2020, time.September, 8)
+	departure := NewDate(2020, time.September, 11)
+	booking := Booking{id: 0, name: "Berlin", arrival: arrival, departure: departure}
 
-// }
+	rooms := []Room{"Rio", "Berlin"}
+	hotel := Hotel{rooms}
+	err := hotel.BookARoom(booking)
+	assert.NoError(t, err)
+	availableRooms := hotel.GetFreeRooms(arrival, departure)
+	assert.Equal(t, []Room{"Rio"}, availableRooms)
+}
