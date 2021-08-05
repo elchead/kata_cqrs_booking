b3f6c79c5f03ea8282eb3c657a5a5debb93b6c0e
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-08-05 20:48:19 +0200

    add test suite

--- room_test.go
+++ room_test.go
@@ -5,90 +5,81 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestShowFreeRooms(t *testing.T) {
-	rooms := []Room{"Rio", "Berlin"}
-	arrival := NewDate(2020, time.August, 13)
-	departure := NewDate(2020, time.August, 16)
-	hotel := NewHotel(rooms)
-	availableRooms := hotel.GetFreeRooms(arrival, departure)
-	assert.Equal(t, rooms, availableRooms)
+type BookingTests struct {
+	suite.Suite
+	rooms     []Room
+	arrival   Date
+	departure Date
+	hotel     Hotel
 }
 
-func TestBookRoom(t *testing.T) {
-	arrival := NewDate(2020, time.September, 8)
-	departure := NewDate(2020, time.September, 11)
-	booking := Booking{id: 0, room: "Berlin", arrival: arrival, departure: departure}
-
-	rooms := []Room{"Rio", "Berlin"}
-	hotel := NewHotel(rooms)
-	err := hotel.BookARoom(booking)
-	assert.NoError(t, err)
-	assert.Equal(t, []Booking{booking}, hotel.bookings)
+func (suite *BookingTests) SetupTest() {
+	suite.rooms = []Room{"Rio", "Berlin"}
+	suite.arrival = NewDate(2020, time.August, 1)
+	suite.departure = NewDate(2020, time.August, 31)
+	suite.hotel = NewHotel(suite.rooms)
 }
 
-func TestNoDoubleBooking(t *testing.T) {
-	arrival := NewDate(2020, time.September, 8)
-	departure := NewDate(2020, time.September, 11)
-	booking := Booking{id: 0, room: "Berlin", arrival: arrival, departure: departure}
+func (suite *BookingTests) TestShowFreeRooms(t *testing.T) {
+	availableRooms := suite.hotel.GetFreeRooms(suite.arrival, suite.departure)
+	assert.Equal(t, suite.rooms, availableRooms)
+}
 
-	rooms := []Room{"Rio", "Berlin"}
-	hotel := NewHotel(rooms)
-	err := hotel.BookARoom(booking)
+func (suite *BookingTests) TestBookRoom(t *testing.T) {
+	booking := Booking{id: 0, room: "Berlin", arrival: suite.arrival, departure: suite.departure}
+	err := suite.hotel.BookARoom(booking)
 	assert.NoError(t, err)
-	err = hotel.BookARoom(booking)
+	assert.Equal(t, []Booking{booking}, suite.hotel.bookings)
+}
+
+func (suite *BookingTests) TestNoDoubleBooking(t *testing.T) {
+	booking := Booking{id: 0, room: "Berlin", arrival: suite.arrival, departure: suite.departure}
+	err := suite.hotel.BookARoom(booking)
+	assert.NoError(t, err)
+	err = suite.hotel.BookARoom(booking)
 	assert.Error(t, err)
 }
 
-func TestFailBookingOverlappingDays(t *testing.T) {
-	arrival := NewDate(2020, time.September, 1)
-	departure := NewDate(2020, time.September, 31)
-	booking := Booking{id: 0, room: "Berlin", arrival: arrival, departure: departure}
-
-	rooms := []Room{"Rio", "Berlin"}
-	hotel := NewHotel(rooms)
-	err := hotel.BookARoom(booking)
+func (suite *BookingTests) TestFailBookingOverlappingDays(t *testing.T) {
+	booking := Booking{id: 0, room: "Berlin", arrival: suite.arrival, departure: suite.departure}
+	err := suite.hotel.BookARoom(booking)
 	assert.NoError(t, err)
-	assert.Equal(t, []Room{"Rio"}, hotel.GetFreeRooms(arrival.AddDate(0, 0, -5), departure.AddDate(0, 0, -5)))
-	booking2 := Booking{id: 1, room: "Berlin", arrival: arrival.AddDate(0, 0, -5), departure: departure.AddDate(0, 0, -5)}
-	err = hotel.BookARoom(booking2)
+
+	arrival2 := suite.arrival.AddDate(0, 0, -5)
+	departure2 := suite.departure.AddDate(0, 0, -5)
+	assert.Equal(t, []Room{"Rio"}, suite.hotel.GetFreeRooms(arrival2, departure2))
+
+	booking2 := Booking{id: 1, room: "Berlin", arrival: arrival2, departure: departure2}
+	err = suite.hotel.BookARoom(booking2)
 	assert.Error(t, err)
 }
 
-func TestFailBookingInvalidRoom(t *testing.T) {
-	arrival := NewDate(2020, time.September, 8)
-	departure := NewDate(2020, time.September, 11)
-	booking := Booking{id: 0, room: "Amsterdam", arrival: arrival, departure: departure}
-	rooms := []Room{"Rio", "Berlin"}
-	hotel := NewHotel(rooms)
-	err := hotel.BookARoom(booking)
+func (setup *BookingTests) TestFailBookingInvalidRoom(t *testing.T) {
+	booking := Booking{id: 0, room: "Amsterdam", arrival: setup.arrival, departure: setup.departure}
+	err := setup.hotel.BookARoom(booking)
 	assert.Error(t, err)
 }
 
-func TestBookAndShow(t *testing.T) {
-	arrival := NewDate(2020, time.September, 8)
-	departure := NewDate(2020, time.September, 11)
-	booking := Booking{id: 0, room: "Berlin", arrival: arrival, departure: departure}
+func (suite *BookingTests) TestBookAndShowFreeRooms(t *testing.T) {
+	booking := Booking{id: 0, room: "Berlin", arrival: suite.arrival, departure: suite.departure}
 
-	rooms := []Room{"Rio", "Berlin"}
-	hotel := NewHotel(rooms)
-	err := hotel.BookARoom(booking)
+	err := suite.hotel.BookARoom(booking)
 	assert.NoError(t, err)
-	availableRooms := hotel.GetFreeRooms(arrival, departure)
+	availableRooms := suite.hotel.GetFreeRooms(suite.arrival, suite.departure)
 	assert.Equal(t, []Room{"Rio"}, availableRooms)
 }
 
-func TestUnaffectedBookingAndFreeRooms(t *testing.T) {
+func (suite *BookingTests) TestUnaffectedBookingAndFreeRooms(t *testing.T) {
 	booking := Booking{id: 0, room: "Berlin", arrival: NewDate(2020, time.January, 1), departure: NewDate(2020, time.May, 13)}
 
-	rooms := []Room{"Rio", "Berlin"}
 	arrival := NewDate(2020, time.August, 13)
 	departure := NewDate(2020, time.August, 16)
-	hotel := NewHotel(rooms)
-	assert.NoError(t, hotel.BookARoom(booking))
-	availableRooms := hotel.GetFreeRooms(arrival, departure)
-	assert.Equal(t, rooms, availableRooms)
+	assert.NoError(t, suite.hotel.BookARoom(booking))
+	availableRooms := suite.hotel.GetFreeRooms(arrival, departure)
+	assert.Equal(t, suite.rooms, availableRooms)
 }
 
 func TestGetBookedRoom(t *testing.T) {
